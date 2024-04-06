1031795bed5ae5ef6f43edc4a7cfdaa2f987c6f3
Béranger GUEDOU <beranger@datakori.tech>
2024-04-06 08:44:41 +0200

    feat(model/training): adds training and validation logic

--- celebrity_recognition_ai/ml/engine.py
+++ celebrity_recognition_ai/ml/engine.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 import os
+
+import mlflow
 import numpy as np
 import torch
-import mlflow
 
 
 def one_epoch_training(dataloader, model, criterion, optimizer, device, breakpoint=20):
-    """
-    """
+    """ """
     model.train()
     train_loss = 0.0
     for i, batch in enumerate(dataloader):
@@ -31,8 +32,7 @@ def one_epoch_training(dataloader, model, criterion, optimizer, device, breakpoi
 
 
 def one_epoch_validation(dataloader, model, criterion, device, breakpoint=4):
-    """
-    """
+    """ """
 
     model.eval()
 
@@ -52,43 +52,65 @@ def one_epoch_validation(dataloader, model, criterion, device, breakpoint=4):
             val_loss += loss.item()
     return val_loss / len(dataloader)
 
-def all_epochs_training_and_validation(logger, train_dataloader, val_dataloader,
-                                       model, criterion, optimizer, device,
-                                       nb_epochs=20, early_stopping=5,
-                                       model_name='basmatinet.pth', breakpoint=2):
-    """
-    """
+
+def all_epochs_training_and_validation(
+    logger,
+    train_dataloader,
+    val_dataloader,
+    model,
+    criterion,
+    optimizer,
+    device,
+    nb_epochs=20,
+    early_stopping=5,
+    model_name="basmatinet.pth",
+    breakpoint=2,
+):
+    """ """
     with mlflow.start_run():
         counter = 0
         best_val_loss = np.inf
         # Tracking all parameters in mlflow
-        mlflow.log_param('nb_epochs', nb_epochs)
-        mlflow.log_param('batch_size', train_dataloader.batch_size)
-        mlflow.log_param('early_stopping', early_stopping)
+        mlflow.log_param("nb_epochs", nb_epochs)
+        mlflow.log_param("batch_size", train_dataloader.batch_size)
+        mlflow.log_param("early_stopping", early_stopping)
 
-        logger.info('Starting a training for {} epochs, with batch size {} and early stopping programmed after {} '.format(
-            nb_epochs, train_dataloader.batch_size, early_stopping))
+        logger.info(
+            "Starting a training for {} epochs, with batch size {} and early stopping programmed after {} ".format(
+                nb_epochs, train_dataloader.batch_size, early_stopping
+            )
+        )
 
         for epoch in range(nb_epochs):
             train_loss = one_epoch_training(
-                train_dataloader, model, criterion, optimizer, device, breakpoint=breakpoint)
+                train_dataloader,
+                model,
+                criterion,
+                optimizer,
+                device,
+                breakpoint=breakpoint,
+            )
             val_loss = one_epoch_validation(
-                val_dataloader, model, criterion, device, breakpoint=breakpoint)
+                val_dataloader, model, criterion, device, breakpoint=breakpoint
+            )
             # Tracking in mlflow
-            mlflow.log_metric('train_loss', train_loss, epoch+1)
-            mlflow.log_metric('val_loss', val_loss, epoch+1)
+            mlflow.log_metric("train_loss", train_loss, epoch + 1)
+            mlflow.log_metric("val_loss", val_loss, epoch + 1)
             # Print metrics
-            logger.debug('Epoch:{}, Train Loss: {}  Val Loss: {} '.format(
-                epoch + 1, round(train_loss, 8), round(val_loss, 8)))
+            logger.debug(
+                "Epoch:{}, Train Loss: {}  Val Loss: {} ".format(
+                    epoch + 1, round(train_loss, 8), round(val_loss, 8)
+                )
+            )
             # Early stopping
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
                 counter = 0
                 # Save the model
-                save_path = os.path.join('basmatinet/app', model_name)
+                save_path = os.path.join("basmatinet/app", model_name)
                 torch.save(model.state_dict(), save_path)
             else:
                 counter += 1
             if counter == early_stopping:
-                logger.debug('======== EARLY STOPPING ========')
+                logger.debug("======== EARLY STOPPING ========")
                 break
