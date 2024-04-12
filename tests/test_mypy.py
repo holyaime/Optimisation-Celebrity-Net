66f4df5cd7ebb1ff185dec496b6f281916a8bc16
yesso-aho <yesso@datakori.tech>
2024-04-12 16:49:52 +0000

    docs: comment test function test_add_with__execpted

--- tests/test_mypy.py
+++ tests/test_mypy.py
@@ -3,6 +3,11 @@ from mypy import api
 
 
 def test_add_with_execpted_args():
+    """
+    Here, we test add function with excepted types for args and
+    we check if exit_status returns by api.run is equal to 0...
+    NB : Function api.run returns a tuple[str, str, int], namely (<normal_report>, <error_report>, <exit_status>)
+    """
     result = api.run(["./for_mypy/add_with_execpted_args.py"])
     assert result[2] == 0
 
